97276d0bd7029f73acbc401895caf990801ea483
Henrique Andrade <h.andrade.rodri@gmail.com>
2025-06-29 19:12:22 -0300

    init repo

---
+++ ProjetoContatosBackEnd/adjenda/src/main/java/com/adjenda/adjenda/AdjendaApplication.java
@@ -0,0 +1,13 @@
+package com.adjenda.adjenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdjendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdjendaApplication.class, args);
+	}
+
+}
---
+++ ProjetoContatosBackEnd/adjenda/src/main/java/com/adjenda/adjenda/controller/Entities.java
@@ -0,0 +1,8 @@
+package com.adjenda.adjenda.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Entities {
+    
+}
