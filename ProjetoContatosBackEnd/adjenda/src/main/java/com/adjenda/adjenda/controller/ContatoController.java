8f921c7635fe339e997ec1e2ea8a07b22fc5c96b
Henrique Andrade <h.andrade.rodri@gmail.com>
2025-06-29 22:26:12 -0300

    feat: Implemented ContatoController with basic CRUD operations

--- ProjetoContatosBackEnd/adjenda/src/main/java/com/adjenda/adjenda/controller/ContatoController.java
+++ ProjetoContatosBackEnd/adjenda/src/main/java/com/adjenda/adjenda/controller/ContatoController.java
@@ -1,32 +1,62 @@
 package com.adjenda.adjenda.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.adjenda.adjenda.models.Contato;
+
+import jakarta.annotation.PostConstruct;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
 public class ContatoController {
     
-    @GetMapping("contato")
-    public Contato getContatos() {
-        
-        Contato contato = new Contato();
+    private List<Contato> contatos = new ArrayList<>();
 
-        return contato;
+    @PostConstruct
+    public void init(){
+        Contato contato1 = new Contato();
+        contato1.setNome("João Silva");
+        contato1.setEmail("joao.silva@email.com");
+        contato1.setTelefone("11999999999");
+
+        Contato contato2 = new Contato();
+        contato2.setNome("Maria Souza");
+        contato2.setEmail("maria.souza@email.com");
+        contato2.setTelefone("11888888888");
+
+        Contato contato3 = new Contato();
+        contato3.setNome("Carlos Pereira");
+        contato3.setEmail("carlos.pereira@email.com");
+        contato3.setTelefone("11777777777");
+
+        contatos.add(contato1);
+        contatos.add(contato2);
+        contatos.add(contato3);
+    }
+    
+    @GetMapping("contatos/{id}")
+    public ResponseEntity<Contato> getContato(@PathVariable int id) {
+        
+        if (id <= contatos.size()) {
+            return ResponseEntity.ok(contatos.get(id - 1)); //O ResponseEntity serve para retornar um erro HTTP na parte do cliente, não mais no servidor
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact not found");     }
+        
+
     }
 
     @GetMapping("contatos")
     public List<Contato> getAllContatos() {
-    
-        List<Contato> listContatos = new ArrayList<>();
-
-        return listContatos;
+        return contatos;
     }
 
 }
