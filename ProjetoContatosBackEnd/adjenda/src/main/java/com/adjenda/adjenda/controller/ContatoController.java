f37f1cd74da76a6ebe22f0cd83f03a752b1f0db4
Henrique Andrade <h.andrade.rodri@gmail.com>
2025-06-29 21:21:36 -0300

    feat: contatos List and single endpoint created

--- ProjetoContatosBackEnd/adjenda/src/main/java/com/adjenda/adjenda/controller/ContatoController.java
+++ ProjetoContatosBackEnd/adjenda/src/main/java/com/adjenda/adjenda/controller/ContatoController.java
@@ -4,19 +4,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.adjenda.adjenda.models.Contato;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class ContatoController {
     
-    @GetMapping("contacts")
+    @GetMapping("contato")
     public Contato getContatos() {
         
         Contato contato = new Contato();
 
         return contato;
     }
+
+    @GetMapping("contatos")
+    public List<Contato> getAllContatos() {
     
+        List<Contato> listContatos = new ArrayList<>();
+
+        return listContatos;
+    }
 
 }
+
+
+
